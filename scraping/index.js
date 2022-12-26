35828fd9114410907588473ae97e5ee27618ec30
Mamun1646 <mimbd01130004@gmail.com>
2022-12-27 01:52:01 +0600

    Traverse Media scrapping

---
+++ scraping/index.js
@@ -0,0 +1,66 @@
+const puppeteer= require ('puppeteer');
+const fs = require('fs');
+async function run(){
+    const browser=await puppeteer.launch();
+    const page =await browser.newPage();
+    await page.goto('https://www.traversymedia.com')
+    // await page.screenshot({path:"emaple.png",fullPage:true})
+   // await page.pdf({path:"example.pdf",format:'A4'})
+   //const html =await page.content()
+ //  const title= await page.evaluate(()=>document.title)
+//console.log(title)
+//const text =await page.evaluate(()=>document.body.innerText)
+// try{const links=await page.evaluate(()=>
+// Array.from(document.querySelectorAll('a'),(e)=>e.href))
+// console.log(links)
+
+// }catch(err) {console.log(err)}
+//     await browser.close()
+// }
+
+
+
+  //  Get courses
+  // const courses = await page.evaluate(() =>
+  //   Array.from(document.querySelectorAll('#courses .card'), (e) => ({
+  //     title: e.querySelector('.card-body h3').innerText,
+  //     level: e.querySelector('.card-body .level').innerText,
+  //     url: e.querySelector('.card-footer a').href,
+  //     promo: e.querySelector('.card-footer .promo-code .promo').innerText,
+  //   }))
+  // );
+// try{const courses=await page.evaluate(()=>
+//     Array.from(document.querySelectorAll('#courses .card'),(e)=>({
+//         //title: e.querySelector('.card-body h3').innerText,
+//        title:e.querySelector('.card-body h3').innerText,
+//         level: e.querySelector('.card-body .level').innerText,
+//         url: e.querySelector('.card-footer a').href,
+//         promo: e.querySelector('.card-footer .promo-code .promo').innerText,
+
+//     })))
+//     console.log(courses)
+    
+//     }catch(err) {console.log(err)}
+
+
+
+     // Get courses using $$eval
+  const courses = await page.$$eval('#courses .card', (elements) =>
+  elements.map((e) => ({
+    title: e.querySelector('.card-body h3').innerText,
+    level: e.querySelector('.card-body .level').innerText,
+    url: e.querySelector('.card-footer a').href,
+    promo: e.querySelector('.card-footer .promo-code .promo').innerText,
+  }))
+);
+
+console.log(courses);
+
+// Save data to JSON file
+fs.writeFile('courses.json', JSON.stringify(courses), (err) => {
+  if (err) throw err;
+  console.log('File saved');
+});
+        await browser.close()
+    }
+run()
